565cbcf9c70daaadf0da61c15e8699ff530348a5
bartick <bartickmaiti1998@gmail.com>
2024-11-16 12:03:06 +0530

    add: zig lsp

--- init.lua
+++ init.lua
@@ -635,6 +635,7 @@ require('lazy').setup({
         dockerls = {},
         markdown_oxide = {},
         mdx_analyzer = {},
+        zls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
